9f5a5e851d642032155b3205c17307ac4e5381f7
Sergey Sergeev <Sergeev_sergey99@mail.ru>
2025-02-24 20:03:53 +0300

    Update Source.cpp

--- Stars/Source.cpp
+++ Stars/Source.cpp
@@ -1,22 +1,28 @@
-﻿#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
 using namespace sf;
 
 const int WINDOW_WIDTH = 1920;
 const int WINDOW_HEIGHT = 1080;
+const int MAX_DISTANCE = 2000;
+const int MAX_OFFSET = 500;
+const int STARS_COUNT = 1000;
+const int MAX_RADIUS = 10;
+
 
 struct Camera
 {
-	float length;
-	float scale = 10;
+	float L;
+	float scale;
 
-	Camera(float l)
+	Camera(float l, float s)
 	{
-		length = l;
+		L = l;
+		scale = s;
 	}
 
 	Vector2f Projection(Vector3f point)
 	{
-		float K = (point.x + length) / length;
+		float K = (L + point.x) / L;
 
 		Vector2f screenPoint;
 		screenPoint.y = WINDOW_HEIGHT - (point.y / K * scale + WINDOW_HEIGHT / 2);
@@ -30,66 +36,83 @@ struct Camera
 int main()
 {
 	RenderWindow window(VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Stars", Style::Fullscreen);
-	window.setFramerateLimit(30);
+	window.setFramerateLimit(60);
 
-	Camera camera(200);
+	Camera camera(200, 10);
 
-	CircleShape shape(10);
-	float speed = 10;
+	Vector3f points3d[STARS_COUNT];
 
-
-	Vector3f points3d[1000];
-	for (int i = 0; i < 1000; i++)
+	for (int i = 0; i < STARS_COUNT; i++)
 	{
-		points3d[i].x = 100 + rand() % 2000;
-		points3d[i].y = -300 + rand() % 600;
-		points3d[i].z = -300 + rand() % 600;
+		points3d[i].x = rand() % MAX_DISTANCE;
+		points3d[i].y = -MAX_OFFSET + rand() % (MAX_OFFSET * 2 + 1);
+		points3d[i].z = -MAX_OFFSET + rand() % (MAX_OFFSET * 2 + 1);
 	}
+
+	CircleShape shape;
+
+	float speed = 10;
 
 	while (window.isOpen())
 	{
-		Event event;
+		sf::Event event;
 		while (window.pollEvent(event))
 		{
 			if (event.type == Event::Closed)
 				window.close();
+
 			if (event.type == Event::KeyPressed)
 			{
-				if (event.key.code == Keyboard::Up){
-					camera.length -= 10;
+				if (event.key.code == Keyboard::Up)
+				{
+					camera.L -= 100;
 				}
-				if (event.key.code == Keyboard::Down){
-					camera.length += 10;
+				if (event.key.code == Keyboard::Down)
+				{
+					camera.L += 100;
 				}
-				if (event.key.code == Keyboard::Left){
-					camera.scale -= 1;
+				if (event.key.code == Keyboard::Left)
+				{
+					camera.scale--;
 				}
-				if (event.key.code == Keyboard::Right){
-					camera.scale += 1;
+				if (event.key.code == Keyboard::Right)
+				{
+					camera.scale++;
 				}
-				if (event.key.code == Keyboard::PageUp){
+				if (event.key.code == Keyboard::PageUp)
+				{
 					speed++;
 				}
-				if (event.key.code == Keyboard::PageDown){
+				if (event.key.code == Keyboard::PageDown)
+				{
 					speed--;
+				}
+				if (event.key.code == Keyboard::Escape)
+				{
+					window.close();
 				}
 			}
 		}
 
-		for (int i = 0; i < 1000; i++)
+		for (int i = 0; i < STARS_COUNT; i++)
 		{
-			points3d[i].x-= speed;
+			points3d[i].x -= speed;
 			if (points3d[i].x <= 0)
-				points3d[i].x += 2000;
+			{
+				points3d[i].x = MAX_DISTANCE;
+			}
+			if (points3d[i].x > MAX_DISTANCE)
+			{
+				points3d[i].x = 0;
+			}
 		}
 
-
 		window.clear();
-		for (int i = 0; i < 1000; i++)
+
+		for (int i = 0; i < STARS_COUNT; i++)
 		{
-			Vector2f screenPoint = camera.Projection(points3d[i]);
-			shape.setPosition(screenPoint);
-			shape.setRadius(10 - 10 * (points3d[i].x / 2000));
+			shape.setPosition(camera.Projection(points3d[i]));
+			shape.setRadius(MAX_RADIUS - MAX_RADIUS * points3d[i].x / MAX_DISTANCE);
 			window.draw(shape);
 		}
 		window.display();
